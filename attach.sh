442d5a6febd9cca855cb741731aa6c0fccc1592a
Daniel Fichtinger <daniel@ficd.ca>
2024-12-29 18:28:44 -0500

    it do be kinda working doe

--- attach.sh
+++ attach.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [[ "$#" -eq 1 ]]; then
+  ZMK_CONTAINER_ID="$1"
+fi
 if [[ -n "$ZMK_CONTAINER_ID" && "$ZMK_CONTAINER_ID" != "null" ]]; then
   echo "Attaching to container ID: $ZMK_CONTAINER_ID"
   docker exec -w /workspaces/zmk -it "$ZMK_CONTAINER_ID" /bin/bash
